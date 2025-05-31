4af0d19915907f2d95f95d7df5a87e85d0ddaebf
EfeBostancii <46efebostancii@gmail.com>
2025-05-31 03:57:18 +0300

    asdddddd

--- src/main/java/com/efe/okys/okysapi/controller/TestController.java
+++ src/main/java/com/efe/okys/okysapi/controller/TestController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-
     @GetMapping("/")
     public String home() {
-        return "OKYS API çalışıyor 🚀";
+        return "OKYS API is up!";
     }
 }
+
